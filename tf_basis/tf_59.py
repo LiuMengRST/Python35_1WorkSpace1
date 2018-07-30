cc5b96f70906c3d7493e314d304f51d9e59c7b1b
LiuMengRST <2548983366@qq.com>
2018-07-30 16:47:18 +0800

    this is a test model

--- tf_basis/tf_59.py
+++ tf_basis/tf_59.py
@@ -1,7 +1,9 @@
 import tensorflow as tf
-x = tf.constant([[0.7, 0.9]])
-1
-11
-111
-
-fu=1
+w1 = tf.Variable(tf.random_normal([2, 3], stddev=1))
+w2 = tf.Variable(tf.random_normal([3, 1], stddev=1))
+x = tf.placeholder(tf.float32, shape=(1, 2), name='input')
+a = tf.matmul(x, w1)
+y = tf.matmul(a, w2)
+sess = tf.InteractiveSession()
+tf.global_variables_initializer().run()
+print(sess.run(y, feed_dict={x: [[0.7, 0.9]]}),)
