d470a15892f5ee34ca5764b1ba81e4b90006d932
LiuMengRST <2548983366@qq.com>
2018-07-31 13:09:37 +0800

    tf_basis

--- Word2Vec/LSMT_PTB.py
+++ Word2Vec/LSMT_PTB.py
@@ -2,6 +2,7 @@ from models.tutorials.rnn.ptb import reader
 import time
 import numpy as np
 import tensorflow as tf
+from tensorflow.contrib.legacy_seq2seq.python.ops.seq2seq import sequence_loss_by_example
 
 class PTBInput(object):
 
@@ -22,13 +23,12 @@ class PTBModel(object):
         vocab_size = config.vocab_size
 
         def lstm_cell():
-            return tf.contrib.rnn.BasicLStMCell(size, forget_bias=0.0, stat_is_tuple=True)
-
+            return tf.nn.rnn_cell.BasicLSTMCell(size, forget_bias=0.0, state_is_tuple=True)
         attn_cell = lstm_cell
         if is_training and config.keep_prob<1:
             def attn_cell():
-                return tf.contrib.rnn.DropoutWrapper(lstm_cell(), oout_keep_prob=config.keep_prob)
-        cell = tf.contrib.rnn.MultiRnnCell([attn_cell() for _ in range(config.num_layers)],state_is_tuple=True)
+                return tf.nn.rnn_cell.MultiRNNCell(lstm_cell(), out_keep_prob=config.keep_prob)
+        cell = tf.nn.rnn_cell.MultiRNNCell([attn_cell() for _ in range(config.num_layers)],state_is_tuple=True)
         self._initial_state = cell.zero_state(batch_size, tf.float32)
 
         embedding = tf.get_variable("embedding", [vocab_size, size], dtype=tf.float32)
@@ -49,9 +49,9 @@ class PTBModel(object):
         softmax_w = tf.get_variable("softmax_w",[size, vocab_size], dtype=tf.float32)
         softmax_b = tf.get_variable("softmax_b", [vocab_size], dtype=tf.float32)
         logits = tf.matmul(output, softmax_w) +softmax_b
-        loss = tf.contrib.legacy_seq2seq.sequenct_loss_by_example([logits], [tf.reshape(input_.targets,[-1])],[tf.ones([batch_size * num_steps],dtype=tf.float32)])
+        loss = sequence_loss_by_example([logits], [tf.reshape(input_.targets,[-1])],[tf.ones([batch_size * num_steps],dtype=tf.float32)])
         self._cost=cost=tf.reduce_sum(loss) / batch_size
-        self._finam_statl = state
+        self._final_state = state
 
         if not is_training:
             return
@@ -64,34 +64,34 @@ class PTBModel(object):
         self._new_lr = tf.placeholder(tf.float32, shape=[], name="new_learning_rate")
         self._lr_update = tf.assign(self._lr, self._new_lr)
 
-        def assign_lr(self, session, lr_value):
-            session.run(self._lr_update, feed_dict={self._new_lr:lr_value})
+    def assign_lr(self, session, lr_value):
+        session.run(self._lr_update, feed_dict={self._new_lr:lr_value})
 
-        @property
-        def input(self):
+    @property
+    def input(self):
             return self._input
 
-        @property
-        def initial_state(self):
+    @property
+    def initial_state(self):
             return self._initial_state
 
-        @property
-        def cost(self):
+    @property
+    def cost(self):
             return self._cost
 
-        @property
-        def final_state(self):
-            return self._final_state
+    @property
+    def final_state(self):
+        return self._final_state
 
-        @property
-        def lr(self):
-            return self._lr
+    @property
+    def lr(self):
+        return self._lr
 
-        @property
-        def train_op(self):
-            return self._train_op
+    @property
+    def train_op(self):
+        return self._train_op
 
-class SamllConfig(object):
+class SmallConfig(object):
     init_scale = 0.1
     learning_rate =1.0
     max_grad_norm = 5
@@ -118,3 +118,78 @@ class TestConfig(object):
     lr_decay = 0.5
     batch_size = 20
     vocab_size = 10000
+
+def run_epoch(session, model, eval_op=None, verbose=False):
+    start_time = time.time()
+    costs = 0.0
+    iters = 0
+    state = session.run(model.initial_state)
+
+    fetches = {
+        'cost': model.cost,
+        'final_state': model.final_state,
+    }
+
+    if eval_op is not None:
+        fetches['eval_op']  = eval_op
+
+    for step in range(model.input.epoch_size):
+        feed_dict = {}
+        for i,(c, h) in enumerate(model.initial_state):
+            feed_dict[c] = state[i].c
+            feed_dict[h] = state[i].h
+
+        vals = session.run(fetches, feed_dict)
+        cost = vals['cost']
+        state = vals['final_state']
+
+
+        cost += vals['cost']
+        iters += model.input.num_steps
+
+        if verbose and step % (model.input.epoch_size//10) == 10:
+            print("%.3f perplexity: %.3f speed: %.0f wps"%(step*1.0/model.input.epoch_size, np.exp(costs / iters),iters * model.input.batch_size/(time.time()-start_time)))
+    return np.exp(costs / iters)
+
+raw_data = reader.ptb_raw_data('simple-examples\\data\\')
+train_data, valid_data, test_data, _=raw_data
+
+config = SmallConfig()
+eval_config = SmallConfig()
+eval_config.batch_size = 1
+eval_config.num_steps = 1
+with tf.Graph().as_default():
+    initializer = tf.random_uniform_initializer(-config.init_scale, config.init_scale)
+
+    with tf.name_scope('Train'):
+        train_input = PTBInput(config = config, data=train_data,name="TrainInput")
+        with tf.variable_scope("Model", reuse=None, initializer=initializer):
+            m = PTBModel(is_training=True, config=config, input_=train_input)
+
+        with tf.name_scope("Valid"):
+            valid_input = PTBInput(config=config, data = valid_data, name="VvalidInput")
+            with tf.variable_scope("Model", reuse=True, initializer=initializer):
+                mvalid = PTBModel(is_training=False, config=config, input_=valid_input)
+
+    with tf.name_scope("Test"):
+        test_input = PTBInput(config=eval_config, data=test_data, name="TestInput")
+        with tf.variable_scope("Model", reuse=True, initializer=initializer):
+            mtest = PTBModel(is_training=False, config=eval_config, input_=test_input)
+
+    sv = tf.train.Supervisor()
+    with sv.managed_session() as session:
+        for i in range(config.max_max_epoch):
+            lr_decay = config.lr_decay ** max(i + 1 - config.max_epoch, 0.0)
+            m.assign_lr(session, config.learning_rate*lr_decay)
+
+            print("Epoch: %d Learning rate: %.3f"%(i+1,session.run(m.lr)))
+            train_perplexity = run_epoch(session, m, eval_op=m._train_op,verbose=True)
+
+            print("Epoch: %d Train Perplexity: %.3f "%(i + 1, train_perplexity))
+            valid_perplexity = run_epoch(session, mvalid)
+            print('Epoch: %d Valid Perplexity: %.3f'%(i + 1,valid_perplexity))
+
+        test_perplexity = run_epoch(session, mtest)
+        print("Test Perplexity: %.3f" %test_perplexity)
+
+
---
+++ test/1.py
@@ -0,0 +1,4 @@
+from temp import a
+from temp import a
+print(a.cls.c)
+print(a.c)
